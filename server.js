bbed3f3d9b098d048441193559948690d4026d38
sohamkoli29 <sohamkoli29@gmaol.com>
2025-12-03 21:11:28 +0530

    production

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ try {
 }
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN ,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
